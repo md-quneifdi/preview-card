95beeccafd02dfdd4bc991fa3dacea1fd1447617
mohammed quneifdi <alladqanymhmd@gmail.com>
2022-06-25 01:43:39 +0300

    ites done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section"></div>
+      <div className="preview-card">
+        <div className="text-part">
+          <h2>Get <span>insights</span> that help your business grow.</h2>
+          <p>Discover the benefits of data analytics and
+            make better decisions regarding revenue, customer
+            experience, and overall efficieny.
+          </p>
+          <div className="review-part">
+            <div className="review">
+              <span className="first">10k+</span>
+              <span className="last">COMPANIES</span>
+            </div>
+            <div className="review">
+              <span className="first">314</span>
+              <span className="last">TEMPLATES</span>
+            </div>
+            <div className="review">
+              <span className="first">12M+</span>
+              <span className="last">QUERIES</span>
+            </div>
+          </div>
+        </div>
+        <div className="image-part">
+          <img src="images/image-header-desktop.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
